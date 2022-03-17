494a761e072979d4639595223d0428bad95ea3ee
mdanialr <mdanialrma@gmail.com>
2022-03-17 17:14:16 +0700

    feat: create folder (pCloud)

--- internal/provider/pcloud/pcloud.go
+++ internal/provider/pcloud/pcloud.go
@@ -7,7 +7,7 @@ const (
 	Login                    = "userinfo"                // Generate token.
 	Logout                   = "logout"                  // Gets a token and invalidates it (Delete a token).
 	TOKENS                   = "listtokens"              // Get a list with the currently active tokens associated with the current user.
-	CREATE_FOLDER            = "createfolderifnotexists" // Creates a folder if the folder doesn't exist or returns the existing folder's metadata.
+	CreateFolder             = "createfolderifnotexists" // Creates a folder if the folder doesn't exist or returns the existing folder's metadata.
 	LIST_FOLDERS             = "listfolder"              // Receive data for a folder.
 	DELETE_FOLDER            = "deletefolder"            // Deletes a folder. The folder must be empty.
 	DELETE_FOLDERS_RECURSIVE = "deletefolderrecursive"   // This function deletes files, directories, and removes sharing. Use with extreme care.
@@ -16,9 +16,10 @@ const (
 // StdResponse standard response from pCloud API that always return 'return' that determine
 // whether API call is success or failure. Response with non '0' return value are errors.
 type StdResponse struct {
-	Result         int `json:"result"` // non 0 result is errors.
-	DigestResponse     // Used by digest.
-	TokenResponse      // Used by login API calls to get token for authentication.
-	LogoutResponse     // Used by logout API calls to invalidate token.
-	QuotaResponse      // Used by print storage quota.
+	Result               int `json:"result"` // non 0 result is errors.
+	DigestResponse           // Used by digest.
+	TokenResponse            // Used by login API calls to get token for authentication.
+	LogoutResponse           // Used by logout API calls to invalidate token.
+	QuotaResponse            // Used by print storage quota.
+	CreateFolderResponse     // Used by create folder.
 }
