a602eee7406cc9f8badcec4a19ca3fd614298b33
mdanialr <mdanialrma@gmail.com>
2023-02-10 21:04:11 +0700

    feat: new helper to handle pointer for primitive/builtin types

--- pkg/helper/common.go
+++ pkg/helper/common.go
@@ -5,6 +5,28 @@ import (
 	"time"
 )
 
+// primitiveType general type that contain all primitive/built in types.
+type primitiveType interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64 |
+		float32 | float64 |
+		string | bool
+}
+
+// Def return the default value of the given t if nil otherwise the real value
+// instead.
+func Def[T primitiveType](t *T) (newT T) {
+	if t != nil {
+		return *t
+	}
+	return newT
+}
+
+// Ptr return pointer of the given t.
+func Ptr[T primitiveType](t T) *T {
+	return &t
+}
+
 // LogStart append [BEGN] and the prefix to the given message and return it.
 // Also append newline at the end.
 func LogStart(prefix, message string) string {
